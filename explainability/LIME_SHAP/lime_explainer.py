30f02bdadf329bdfbb1dd04c7c1ef1a07aec5166
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2025-05-11 21:45:50 +0530

    add

--- explainability/LIME_SHAP/lime_explainer.py
+++ explainability/LIME_SHAP/lime_explainer.py
@@ -21,25 +21,27 @@ i = 0  # pick an instance
 exp = explainer.explain_instance(X[i], rf.predict_proba, num_features=4)
 exp.show_in_notebook()
 
-# def explain_with_lime(model, X, feature_names, class_names, num_features=5):
-#     """
-#     Explain the predictions of a model using LIME.  
-#     Args:
-#         model: The trained model to explain.
-#         X: The input data to explain.
-#         feature_names: Names of the features in the dataset.
-#         class_names: Names of the classes in the dataset.
-#         num_features: The number of features to include in the explanation.
+def explain_with_lime(model, X, feature_names, class_names, num_features=5):
 
-#     Returns:
-#         A LIME explanation object.
-#     """
-#     explainer = lime.lime_tabular.LimeTabularExplainer(
-#         training_data=np.array(X),
-#         feature_names=feature_names,
-#         class_names=class_names,
-#         mode='classification'
-#     )
-#     return explainer
+    """
+    Explain the predictions of a model using LIME.  
+    Args:
+        model: The trained model to explain.
+        X: The input data to explain.
+        feature_names: Names of the features in the dataset.
+        class_names: Names of the classes in the dataset.
+        num_features: The number of features to include in the explanation.
+
+    Returns:
+        A LIME explanation object.
+    """
+    explainer = lime.lime_tabular.LimeTabularExplainer(
+        training_data=np.array(X),
+        feature_names=feature_names,
+        class_names=class_names,
+        mode='classification'
+    )
+    
+    return explainer
 
 
